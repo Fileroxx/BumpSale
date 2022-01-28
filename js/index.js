065805eb4660b2c544915c10dae2a3746fdb27f4
Igor Fernandes <igor_cesari@hotmail.com>
2022-01-28 17:55:51 -0300

    2/3 Finalizado

--- js/index.js
+++ js/index.js
@@ -6,3 +6,26 @@ $(document).ready(function(){
     });
 });
 
+
+setTimeout(function() {
+    $('.preloader').fadeToggle();
+},3000)
+
+
+
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabTarget)
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active')
+        })
+        tabs.forEach(tab => {
+            tab.classList.remove('active')
+        })
+        tab.classList.add('active')
+        target.classList.add('active')
+    })
+})
