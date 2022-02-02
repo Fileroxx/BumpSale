d3a16aa018175f1e83d46a8a1d4a83a1d6b63c20
Igor Fernandes <igor_cesari@hotmail.com>
2022-02-02 13:34:00 -0300

    DEBUGGER TABS

--- js/index.js
+++ js/index.js
@@ -41,3 +41,15 @@ faqs.forEach(faq => {
     faq.classList.toggle("active");
   });
 });
+
+
+
+var triggerTabList = [].slice.call(document.querySelectorAll('#myTab a'))
+triggerTabList.forEach(function (triggerEl) {
+  var tabTrigger = new bootstrap.Tab(triggerEl)
+
+  triggerEl.addEventListener('click', function (event) {
+    event.preventDefault()
+    tabTrigger.show()
+  })
+})
